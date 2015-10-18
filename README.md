87b5e7150a3231b5bfbe3d2eeb18f510f89161e3
Bob Zurad <bob@zurad.net>
2015-10-17 21:52:19 -0500

    work done at the Hack & Tell

---
+++ README.md
@@ -0,0 +1,24 @@
+ionic-twitter-test
+==================
+
+A Twitter client powered by Ionic Framework that I presented at the RokkinCat Hack & Tell. This is how incredibly easy it is to setup a project using Ionic Framework.
+
+###Development Environment
+If you've never developed with Node or Ionic, you'll have to follow these steps:
+* Install [node.js](https://nodejs.org/en/)
+* Install [bower](http://bower.io/)
+* Install [ionic command line tools](https://www.npmjs.com/package/ionic)
+* Follow instructions in Cordova's [Android Platform Guide](http://cordova.apache.org/docs/en/5.1.1/guide/platforms/android/index.html)
+  * Stop at "Create New Project"
+* Follow instructions in Cordova's [iOS Platform Guide](http://cordova.apache.org/docs/en/5.1.1/guide/platforms/ios/index.html)
+  * Stop at "Create New Project"
+* Install [ios-sim](https://www.npmjs.com/package/ios-sim)
+
+###Accessing Twitter's API
+To access Twitter's API you must create an application at [apps.twitter.com](https://apps.twitter.com/)
+
+###How I created this project
+* Follow Simon Reimler's instructions in his article [here](http://devdactic.com/twitter-rest-api-angularjs/)
+
+Note: In this project, I've removed my Consumer Secret from app.js
+Note: ngTwitter can't authenticate with a web browser on your local machine. You must run the application in an emulator or deploy it to a device.
---
+++ www/js/app.js
@@ -0,0 +1,64 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+angular.module('starter', ['ionic', 'ngCordova', 'ngTwitter'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    }
+    if(window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+  });
+}).controller('AppCtrl', function($scope, $ionicPlatform, $twitterApi, $cordovaOauth) {
+  var twitterKey = 'STORAGE.TWITTER.KEY';
+  var clientId = 'Qr8Vmw6FDPSSpfRPqiH1YufDc';
+  var clientSecret = '';
+  var myToken = '';
+
+  $scope.tweet = {};
+
+  $ionicPlatform.ready(function() {
+    myToken = JSON.parse(window.localStorage.getItem(twitterKey));
+    if (myToken === '' || myToken === null) {
+      $cordovaOauth.twitter(clientId, clientSecret).then(function (succ) {
+        myToken = succ;
+        window.localStorage.setItem(twitterKey, JSON.stringify(succ));
+        $twitterApi.configure(clientId, clientSecret, succ);
+        $scope.showHomeTimeline();
+      }, function(error) {
+        console.log(error);
+      });
+    } else {
+      $twitterApi.configure(clientId, clientSecret, myToken);
+      $scope.showHomeTimeline();
+    }
+  });
+
+  $scope.showHomeTimeline = function() {
+    $twitterApi.getHomeTimeline().then(function(data) {
+      $scope.home_timeline = data;
+    });
+  };
+
+  $scope.submitTweet = function() {
+    $twitterApi.postStatusUpdate($scope.tweet.message).then(function(result) {
+      $scope.showHomeTimeline();
+    });
+  };
+
+  $scope.doRefresh = function() {
+    $scope.showHomeTimeline();
+    $scope.$broadcast('scroll.refreshComplete');
+  };
+
+  $scope.correctTimestring = function(string) {
+    return new Date(Date.parse(string));
+  };
+});
